3c47b576279f50a47e49d705bbb94c092d017930
niuf <niuffars@pm.me>
2023-05-25 12:28:40 +0200

    Merged conflicts

--- src/main/java/it/academy/AGMExpress/entity/Cliente.java
+++ src/main/java/it/academy/AGMExpress/entity/Cliente.java
@@ -1,4 +1,4 @@
-package entity;
+package it.academy.AGMExpress.entity;
 
 
 import jakarta.persistence.*;
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.antlr.v4.runtime.misc.NotNull;
 
 import java.util.List;
 
--- src/main/java/it/academy/AGMExpress/entity/Indirizzo.java
+++ src/main/java/it/academy/AGMExpress/entity/Indirizzo.java
@@ -1,4 +1,4 @@
-package entity;
+package it.academy.AGMExpress.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
